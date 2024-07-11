6fa90a13ccda9a59b2698e73ca5e22f413405d02
eathyn <eathynzakas@gmail.com>
2024-07-11 15:44:55 +0800

    prevent polluting global scope

--- src/main.js
+++ src/main.js
@@ -1,16 +1,4 @@
-import Student from './student.js'
-
-class Person {
-  constructor(name) {
-    this.name = name
-  }
-  greet() {
-    console.log(`hi, I am ${this.name}`)
-  }
-}
-
-const person = new Person('Tom')
-person.greet()
-
-const student = new Student('Jay', 25)
-student.greet()
+const promise = new Promise((resolve) => resolve(1))
+promise.finally(() => {
+  console.log('success')
+})
--- webpack.config.js
+++ webpack.config.js
@@ -21,15 +21,18 @@ module.exports = {
             presets: [
               [
                 '@babel/preset-env',
-                {
-                  useBuiltIns: 'usage',
-                  corejs: 3.37,
-                },
+                // {
+                //   useBuiltIns: 'usage',
+                //   corejs: 3.37,
+                // },
               ],
             ],
             plugins: [
               [
                 '@babel/plugin-transform-runtime',
+                {
+                  corejs: 3,
+                },
               ],
             ],
           },
